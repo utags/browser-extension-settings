528bdc6912ce0b614a9efb6dc0b20c86f8f731b1
pipecraft <pipecraft.net@gmail.com>
2023-06-14 10:51:42 +0800

    Settings for browser extensions and userscripts

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+export async function initSettings(options: Record<string, unknown>): void
+
+export async function showSettings(): void
+
+export function getSettingsValue(key: string): boolean | string | undefined
